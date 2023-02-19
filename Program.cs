4220780e4082ce88d0c420a4107e81ce6353e796
singhajit4u <ajit.co.info@gmail.com>
2023-02-19 10:36:45 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Area_Of_Square
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the Side of Square:");
+            float x = Single.Parse(Console.ReadLine()); 
+            float result = x * x;
+            Console.WriteLine("The Area of Square:"+result);
+            
+        }
+    }
+}
